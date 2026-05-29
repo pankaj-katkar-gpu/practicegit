dbc71f31df95b85bc690808909a39b0ba05e4da5
apna_college <pankajkatkar263@gmail.com>
2026-05-29 21:38:39 +0530

    changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def divi(a, b):
         return "Cannot divide by zero"
     return a / b
 
-def sub(a, b,c):
-    return a - b - c
+def sub(a, b,d):
+    return a - b - d
 def mul(a, b):
     return a * b
